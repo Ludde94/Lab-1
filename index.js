11cb87c73d43868e81edb40f873e57579583dd48
ludde94 <l.jenisch@hotmail.com>
2023-04-19 15:11:09 +0200

    setup

---
+++ index.js
@@ -0,0 +1,10 @@
+// setup
+
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const mongoDB = "insert_your_database_url_here";
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}

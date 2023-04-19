4d43190d86523ce2bc8c721b9f30bb417748312d
ludde94 <l.jenisch@hotmail.com>
2023-04-19 16:18:30 +0200

    path and index working

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
-// setup
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const path = require('path');
 
-const mongoose = require("mongoose");
-mongoose.set("strictQuery", false);
-const mongoDB = "insert_your_database_url_here";
-
-main().catch((err) => console.log(err));
-async function main() {
-  await mongoose.connect(mongoDB);
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.CONNECTION_URL;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+async function run() {
+    try {
+        // Connect the client to the server    (optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
 }
+run().catch(console.dir);
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '/index.html'));
+})
+
+
+app.listen(process.env.PORT, () => {
+    console.log("Server listening on port: " + process.env.PORT)
+})
